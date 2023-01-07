5bdec4af78994bbbf9c9d7fb008fa0932de08fff
Evrouin <elwaycortez@gmail.com>
2023-01-07 17:26:18 +0800

    added user name for generating loyalty card number

--- loyalty-cards/create.py
+++ loyalty-cards/create.py
@@ -11,14 +11,35 @@ def generate_number():
 
 
 def handler(event, context):
+    # Parse the request body as JSON
+    body = json.loads(event['body'])
+
+    # Check if the request includes the first_name and last_name fields
+    if 'first_name' not in body or 'last_name' not in body:
+        return {
+            'statusCode': 400,
+            'body': json.dumps({'message': 'Missing required fields'}),
+            'headers': {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            },
+        }
+
     try:
         current_time = str(datetime.datetime.now())
         card_number = generate_number()
+
+        # Get the first_name and last_name from the request
+        first_name = body['first_name']
+        last_name = body['last_name']
 
         client.put_item(
             TableName='loyalty-cards',
             Item={
                 'card_number': {'S': card_number},
+                'first_name': {'S': first_name},
+                'last_name': {'S': last_name},
+                'points': {'N': '0'},  # Set points to 0
                 'created_at': {'S': current_time},
             }
         )
@@ -29,6 +50,9 @@ def handler(event, context):
                 'message': 'Card created successfully',
                 'data': {
                     'card_number': card_number,
+                    'first_name': first_name,
+                    'last_name': last_name,
+                    'points': 0,
                     'created_at': current_time,
                 },
             }),
